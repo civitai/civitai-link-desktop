feddfec5a06eff661389542dc7e1efc2e80d5819
Chase Moody <cmoody06@gmail.com>
2024-02-07 15:57:46 -0600

    Update no activity

--- src/renderer/src/components/activity/activity.tsx
+++ src/renderer/src/components/activity/activity.tsx
@@ -1,16 +1,18 @@
 import { useElectron } from '@/providers/electron';
 import { ActivityItem } from './activity-item';
+import { useMemo } from 'react';
 
 export function Activity() {
   const { activityList } = useElectron();
+  const activityKeys = useMemo(() => Object.keys(activityList), [activityList]);
 
-  if (!activityList) {
+  if (activityKeys.length === 0) {
     return <p>No Activity</p>;
   }
 
   return (
     <div>
-      {Object.keys(activityList)?.map((activity) => {
+      {activityKeys?.map((activity) => {
         return <ActivityItem {...activityList[activity]} key={activityList[activity].hash} />;
       })}
     </div>
