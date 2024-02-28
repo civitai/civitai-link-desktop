d0afa24653ff7ed54b02e2b7741834ab12a349a4
Chase Moody <cmoody06@gmail.com>
2024-02-28 09:35:05 -0600

    Sample data for activities

--- src/renderer/src/components/activities/activities-item.tsx
+++ src/renderer/src/components/activities/activities-item.tsx
@@ -1,13 +1,25 @@
 import { DownloadCloud, Trash2 } from 'lucide-react';
 import { ActivityType } from '@/types';
+import dayjs from 'dayjs';
 
-export function ActivitiesItem() {
-  const name = 'SDXL Dragon Style';
-  const type = ActivityType.Downloaded;
+type ActivitiesItemProps = {
+  name: string;
+  type: ActivityType;
+  date: string;
+  civitaiUrl?: string;
+};
 
+export function ActivitiesItem({
+  name,
+  type,
+  date,
+  civitaiUrl,
+}: ActivitiesItemProps) {
   return (
     <div>
-      <p className="text-xs text-[#909296]">2024-02-23</p>
+      <p className="text-xs text-[#909296]">
+        {dayjs(date).format('YYYY-MM-DD')}
+      </p>
       <div className="flex space-x-2 items-center mt-2">
         {type === ActivityType.Downloaded ? (
           <DownloadCloud size={20} />
--- src/renderer/src/components/activities/activities.tsx
+++ src/renderer/src/components/activities/activities.tsx
@@ -1,18 +1,36 @@
-// import { useElectron } from '@/providers/electron';
+import { useElectron } from '@/providers/electron';
 import { ActivitiesItem } from './activities-item';
-// import { useMemo } from 'react';
+import { useMemo } from 'react';
+import { ActivityType } from '@/types';
 
 export function Activities() {
-  // const { activityList } = useElectron();
-  // const activityKeys = useMemo(() => Object.keys(activityList), [activityList]);
+  const { activityList } = useElectron();
+  const activityKeys = useMemo(() => Object.keys(activityList), [activityList]);
 
-  //   if (activityKeys.length === 0) {
-  //     return <p>No Activities</p>;
-  //   }
+  const activities = [
+    {
+      name: 'test',
+      date: '2021-10-10',
+      type: ActivityType.Downloaded,
+      civitaiUrl: 'https://civitai.com',
+    },
+    {
+      name: 'test2',
+      date: '2021-10-10',
+      type: ActivityType.Deleted,
+      civitaiUrl: 'https://civitai.com',
+    },
+  ];
+
+  if (activities.length === 0) {
+    return <p>No Activities</p>;
+  }
 
   return (
-    <div>
-      <ActivitiesItem />
+    <div className="flex flex-col gap-y-4 ">
+      {activities.map((activity, key) => (
+        <ActivitiesItem {...activity} key={`${name}-${key}`} />
+      ))}
     </div>
   );
 }
