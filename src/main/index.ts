7dcce909afad0d811dd8bd4fc5a56ea9d605d70c
Chase Moody <cmoody06@gmail.com>
2024-03-06 10:08:03 -0600

    Revert window positioning function

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,6 +9,7 @@ import {
   Tray,
   nativeImage,
   Menu,
+  screen,
 } from 'electron';
 import { join } from 'path';
 import {
@@ -37,6 +38,8 @@ let tray;
 //defaults
 let width = 400;
 let height = 600;
+let margin_x = 0;
+let margin_y = 0;
 let framed = false;
 
 const DEBUG = import.meta.env.MAIN_VITE_DEBUG === 'true' || false;
@@ -126,18 +129,65 @@ function toggleWindow() {
 }
 
 function alignWindow() {
-  const trayBounds = tray.getBounds();
+  const position = calculateWindowPosition();
   mainWindow.setBounds({
     width: width,
     height: height,
-    x: trayBounds.x,
-    y: trayBounds.y,
+    x: position.x,
+    y: position.y,
   });
 }
 
 function showWindow() {
   alignWindow();
   mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+}
+
+function calculateWindowPosition() {
+  const screenBounds = screen.getPrimaryDisplay().size;
+  const trayBounds = tray.getBounds();
+
+  //where is the icon on the screen?
+  let trayPos = 4; // 1:top-left 2:top-right 3:bottom-left 4.bottom-right
+  trayPos = trayBounds.y > screenBounds.height / 2 ? trayPos : trayPos / 2;
+  trayPos = trayBounds.x > screenBounds.width / 2 ? trayPos : trayPos - 1;
+
+  let DEFAULT_MARGIN = { x: margin_x, y: margin_y };
+  let x;
+  let y;
+
+  //calculate the new window position
+  switch (trayPos) {
+    case 1: // for TOP - LEFT
+      x = Math.floor(trayBounds.x + DEFAULT_MARGIN.x + trayBounds.width / 2);
+      y = Math.floor(trayBounds.y + DEFAULT_MARGIN.y + trayBounds.height / 2);
+      break;
+
+    case 2: // for TOP - RIGHT
+      x = Math.floor(
+        trayBounds.x - width - DEFAULT_MARGIN.x + trayBounds.width / 2,
+      );
+      y = Math.floor(trayBounds.y + DEFAULT_MARGIN.y + trayBounds.height / 2);
+      break;
+
+    case 3: // for BOTTOM - LEFT
+      x = Math.floor(trayBounds.x + DEFAULT_MARGIN.x + trayBounds.width / 2);
+      y = Math.floor(
+        trayBounds.y - height - DEFAULT_MARGIN.y + trayBounds.height / 2,
+      );
+      break;
+
+    case 4: // for BOTTOM - RIGHT
+      x = Math.floor(
+        trayBounds.x - width - DEFAULT_MARGIN.x + trayBounds.width / 2,
+      );
+      y = Math.floor(
+        trayBounds.y - height - DEFAULT_MARGIN.y + trayBounds.height / 2,
+      );
+      break;
+  }
+
+  return { x: x, y: y };
 }
 
 Menu.setApplicationMenu(null);
