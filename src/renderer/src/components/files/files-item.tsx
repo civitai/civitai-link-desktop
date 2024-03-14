1217037d8104c98a4597ffd70bec1eb7adb32f22
Chase Moody <cmoody06@gmail.com>
2024-03-14 18:08:54 -0500

    Add tooltips to file items and vault storage icon

--- src/renderer/src/components/files/files-item.tsx
+++ src/renderer/src/components/files/files-item.tsx
@@ -8,9 +8,17 @@ import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 import { useApi } from '@/hooks/use-api';
-import { DownloadCloud, Image } from 'lucide-react';
+import { DownloadCloud, Image, UploadCloud } from 'lucide-react';
 import { FileItemDelete } from './file-item-delete';
 import { useFile } from '@/providers/files';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@radix-ui/react-tooltip';
+
+// import { CiVault } from "react-icons/ci";
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
@@ -56,91 +64,118 @@ export function FilesItem({ resource }: FilesItemProps) {
   };
 
   return (
-    <Card className="mb-2 bg-transparent group">
-      {!isNotDone ? (
-        <CardContent>
-          <div className="flex relative">
-            {resource.previewImageUrl ? (
-              <div className="w-12 h-12 mr-2 items-center overflow-hidden rounded">
-                <img
-                  src={resource.previewImageUrl}
-                  alt={resource.modelName}
-                  className="h-full w-full object-cover object-center"
-                />
+    <TooltipProvider>
+      <Card className="mb-2 bg-transparent group">
+        {!isNotDone ? (
+          <CardContent>
+            <div className="flex relative">
+              {resource.previewImageUrl ? (
+                <div className="w-12 h-12 mr-2 items-center overflow-hidden rounded relative">
+                  <img
+                    src={resource.previewImageUrl}
+                    alt={resource.modelName}
+                    className="h-full w-full object-cover object-center group-hover:opacity-5"
+                  />
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <UploadCloud className="absolute top-3 left-3 w-6 h-6 cursor-pointer hidden group-hover:flex" />
+                    </TooltipTrigger>
+                    <TooltipContent className="max-w-[360px] bg-background/90 rounded p-1 ml-8 border">
+                      <p className="text-xs">Store resource in your vault</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+              ) : (
+                <div className="bg-card w-12 h-12 mr-2 rounded flex items-center justify-center">
+                  <Image size={24} />
+                </div>
+              )}
+              <div className="w-full whitespace-nowrap overflow-hidden pr-8 justify-between flex flex-col flex-1">
+                <div>
+                  <a href={resource.civitaiUrl} target="_blank">
+                    <p className="text-sm leading-none dark:text-white font-bold text-ellipsis overflow-hidden">
+                      {resource.modelName}
+                    </p>
+                  </a>
+                </div>
+                <div className="flex items-center space-x-2 group-hover:hidden">
+                  <Badge variant="modelTag">{resource.type}</Badge>
+                  <Badge variant="outline">{resource.modelVersionName}</Badge>
+                </div>
+                <div className="justify-center hidden group-hover:flex flex-col">
+                  {resource.downloadDate ? (
+                    <p className="text-[10px] font-normal text-[#909296] flex items-center">
+                      <DownloadCloud
+                        className="mr-1"
+                        size={12}
+                        color="#909296"
+                      />
+                      {dayjs(resource.downloadDate).fromNow()}
+                    </p>
+                  ) : null}
+                  <p
+                    className="text-[10px] dark:text-[#909296] text-ellipsis overflow-hidden cursor-pointer"
+                    onClick={() =>
+                      resource?.localPath
+                        ? openModelFileFolder(resource.localPath)
+                        : alert('Path to file cant be found.')
+                    }
+                  >
+                    {resource.name}
+                  </p>
+                </div>
               </div>
-            ) : (
-              <div className="bg-card w-12 h-12 mr-2 rounded flex items-center justify-center">
-                <Image size={24} />
-              </div>
-            )}
-            <div className="w-full whitespace-nowrap overflow-hidden pr-8 justify-between flex flex-col flex-1">
+              {!isNotDone ? (
+                <Tooltip>
+                  <TooltipTrigger>
+                    <FileItemDelete resource={resource} />
+                  </TooltipTrigger>
+                  <TooltipContent className="max-w-[360px] bg-background/90 rounded p-1 border z-50">
+                    <p className="text-xs">Delete from file system</p>
+                  </TooltipContent>
+                </Tooltip>
+              ) : null}
+            </div>
+          </CardContent>
+        ) : (
+          <CardContent className="space-y-2">
+            <div className="flex justify-between">
+              <a href={resource.civitaiUrl} target="_blank">
+                <p className="text-sm leading-none dark:text-white font-bold text-ellipsis overflow-hidden">
+                  {resource.modelName}
+                </p>
+              </a>
               <div>
-                <a href={resource.civitaiUrl} target="_blank">
-                  <p className="text-sm leading-none dark:text-white font-bold text-ellipsis overflow-hidden">
-                    {resource.modelName}
-                  </p>
-                </a>
-              </div>
-              <div className="flex items-center space-x-2 group-hover:hidden">
-                <Badge variant="modelTag">{resource.type}</Badge>
-                <Badge variant="outline">{resource.modelVersionName}</Badge>
-              </div>
-              <div className="justify-center hidden group-hover:flex flex-col">
-                {resource.downloadDate ? (
-                  <p className="text-[10px] font-normal text-[#909296] flex items-center">
-                    <DownloadCloud className="mr-1" size={12} color="#909296" />
-                    {dayjs(resource.downloadDate).fromNow()}
-                  </p>
-                ) : null}
-                <p
-                  className="text-[10px] dark:text-[#909296] text-ellipsis overflow-hidden cursor-pointer"
-                  onClick={() =>
-                    resource?.localPath
-                      ? openModelFileFolder(resource.localPath)
-                      : alert('Path to file cant be found.')
-                  }
-                >
-                  {resource.name}
+                <p className="text-xs text-[#909296] leading-none">
+                  {prettyBytes(speed)}/s
                 </p>
               </div>
             </div>
-            {!isNotDone ? <FileItemDelete resource={resource} /> : null}
-          </div>
-        </CardContent>
-      ) : (
-        <CardContent className="space-y-2">
-          <div className="flex justify-between">
-            <a href={resource.civitaiUrl} target="_blank">
-              <p className="text-sm leading-none dark:text-white font-bold text-ellipsis overflow-hidden">
-                {resource.modelName}
-              </p>
-            </a>
-            <div>
-              <p className="text-xs text-[#909296] leading-none">
-                {prettyBytes(speed)}/s
-              </p>
-            </div>
-          </div>
-          <div className="flex items-center">
-            <div className="flex w-full items-center">
-              <Progress value={progress} className="mr-2" />
-              <div className="cursor-pointer" onClick={cancelAndRemoveDownload}>
-                <AiOutlineCloseCircle size={16} />
+            <div className="flex items-center">
+              <div className="flex w-full items-center">
+                <Progress value={progress} className="mr-2" />
+                <div
+                  className="cursor-pointer"
+                  onClick={cancelAndRemoveDownload}
+                >
+                  <AiOutlineCloseCircle size={16} />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex-row flex justify-between w-full">
-            <div>
+            <div className="flex-row flex justify-between w-full">
+              <div>
+                <p className="text-xs text-[#909296] leading-none">
+                  {Math.floor(progress)}%
+                </p>
+              </div>
               <p className="text-xs text-[#909296] leading-none">
-                {Math.floor(progress)}%
+                {dayjs.duration({ seconds: remainingTime }).humanize()}{' '}
+                remaining
               </p>
             </div>
-            <p className="text-xs text-[#909296] leading-none">
-              {dayjs.duration({ seconds: remainingTime }).humanize()} remaining
-            </p>
-          </div>
-        </CardContent>
-      )}
-    </Card>
+          </CardContent>
+        )}
+      </Card>
+    </TooltipProvider>
   );
 }
