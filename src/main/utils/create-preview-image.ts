793894b60697bf616d55a7f2b24ab21a616e716f
Chase Moody <cmoody06@gmail.com>
2024-04-10 12:12:46 -0500

    Fix preview image for A1111

---
+++ src/main/utils/create-preview-image.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import fs from 'fs';
+import axios from 'axios';
+
+export async function createPreviewImage(file: Resource) {
+  if (!file.previewImageUrl || !file.localPath) return;
+
+  const previewPath = path.resolve(
+    __dirname,
+    file.localPath?.split('.').slice(0, -1).join('.') + '.preview.png',
+  );
+  const writer = fs.createWriteStream(previewPath);
+
+  const response = await axios({
+    url: file.previewImageUrl,
+    method: 'GET',
+    responseType: 'stream',
+  });
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve);
+    writer.on('error', reject);
+  });
+}
