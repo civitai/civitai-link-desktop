670209695ba476514582be01a74c6a92a22ff689
Chase Moody <cmoody06@gmail.com>
2024-04-16 17:22:05 -0500

    Fix download

--- src/main/download-file.ts
+++ src/main/download-file.ts
@@ -6,7 +6,6 @@ import { BrowserWindow, Notification, ipcMain } from 'electron';
 import { addFile } from './store/files';
 import { updateActivity } from './store/activities';
 import { filterResourcesList } from './commands/filter-reources-list';
-import { getApiKey } from './store/store';
 
 type DownloadFileParams = {
   socket: Socket;
@@ -21,8 +20,6 @@ export async function downloadFile({
   downloadPath,
   resource,
 }: DownloadFileParams) {
-  const apiKey = getApiKey();
-
   console.log('Connecting …');
   const controller = new AbortController();
   const { data, headers } = await axios({
@@ -30,7 +27,6 @@ export async function downloadFile({
     method: 'GET',
     responseType: 'stream',
     signal: controller.signal,
-    headers: apiKey ? { Authorization: `Bearer ${apiKey}` } : {},
   });
   const totalLength = parseInt(headers['content-length'], 10);
 
