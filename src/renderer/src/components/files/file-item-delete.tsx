519cf50d7d4240470c8fa57a1c3a1e6814e651f2
Chase Moody <cmoody06@gmail.com>
2024-04-22 17:24:15 -0500

    Add tooltip to delete model

--- src/renderer/src/components/files/file-item-delete.tsx
+++ src/renderer/src/components/files/file-item-delete.tsx
@@ -11,6 +11,11 @@ import {
 } from '@/components/ui/alert-dialog';
 import { Trash2 } from 'lucide-react';
 import { useApi } from '@/hooks/use-api';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 type FileItemDeleteProps = { resource: Resource };
 
@@ -23,9 +28,14 @@ export function FileItemDelete({ resource }: FileItemDeleteProps) {
 
   return (
     <AlertDialog>
-      <AlertDialogTrigger className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
-        <Trash2 className="h-4 w-4" color="#F15252" />
-      </AlertDialogTrigger>
+      <Tooltip>
+        <AlertDialogTrigger className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10">
+          <TooltipTrigger asChild>
+            <Trash2 className="h-4 w-4" color="#F15252" />
+          </TooltipTrigger>
+        </AlertDialogTrigger>
+        <TooltipContent side="bottom">Delete Model</TooltipContent>
+      </Tooltip>
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
