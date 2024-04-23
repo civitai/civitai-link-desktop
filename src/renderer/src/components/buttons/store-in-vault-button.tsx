55d6413f22d4582000bbb3c9f7e026d8109d310e
Chase Moody <cmoody06@gmail.com>
2024-04-23 12:44:52 -0500

    Add close x to alert dialog

--- src/renderer/src/components/buttons/store-in-vault-button.tsx
+++ src/renderer/src/components/buttons/store-in-vault-button.tsx
@@ -1,4 +1,4 @@
-import { UploadCloud } from 'lucide-react';
+import { UploadCloud, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
   Tooltip,
@@ -61,6 +61,10 @@ export function StoreInVaultButton({ file }: StoreInVaultButtonProps) {
               </AlertDialogTrigger>
               <AlertDialogContent>
                 <AlertDialogHeader>
+                  <AlertDialogCancel className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+                    <X className="h-4 w-4" />
+                    <span className="sr-only">Close</span>
+                  </AlertDialogCancel>
                   <AlertDialogTitle>Delete from file system?</AlertDialogTitle>
                   <AlertDialogDescription>
                     Saving to vault allows you to clear up space locally. We can
