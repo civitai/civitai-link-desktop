765729a1a5566ecbc759d295cad51a27a95164aa
Chase Moody <cmoody06@gmail.com>
2024-04-25 12:38:43 -0500

    Increase timeout and decrease batches

--- src/main/utils/create-preview-image.ts
+++ src/main/utils/create-preview-image.ts
@@ -1,11 +1,9 @@
 import path from 'path';
 import fs from 'fs';
-import electronDl, { download } from 'electron-dl';
-import { getWindow } from '../browser-window';
+import axios from 'axios';
 
 export async function createPreviewImage(file: Resource) {
   if (!file.previewImageUrl || !file.localPath) return;
-
   const previewPath = path.resolve(
     __dirname,
     file.localPath?.split('.').slice(0, -1).join('.') + '.preview.png',
@@ -14,17 +12,13 @@ export async function createPreviewImage(file: Resource) {
   if (fs.existsSync(previewPath)) return;
 
   try {
-    await download(getWindow(), file.previewImageUrl, {
-      directory: path.dirname(previewPath),
-      filename: path.basename(previewPath),
-      showBadge: false,
-      showProgressBar: false,
+    const response = await axios.get(file.previewImageUrl, {
+      responseType: 'arraybuffer',
+      timeout: 5000,
     });
-  } catch (error) {
-    if (error instanceof electronDl.CancelError) {
-      console.info('item.cancel() was called');
-    } else {
-      console.error(error);
-    }
+
+    return await fs.promises.writeFile(previewPath, response.data);
+  } catch (e) {
+    console.error('Error creating preview image', file.previewImageUrl, e);
   }
 }
