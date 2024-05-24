c4b27844ee2efe3e96de428fbc4a31b15454a298
Chase Moody <cmoody06@gmail.com>
2024-05-24 16:01:09 -0500

    Update vault to use download splitting

--- src/main/vault-download.ts
+++ src/main/vault-download.ts
@@ -5,8 +5,74 @@ import path from 'path';
 import { v4 as uuid } from 'uuid';
 import { getWindow } from './browser-window';
 import { getRootResourcePath } from './store/paths';
-import { getApiKey } from './store/store';
+import { getApiKey, getSettings } from './store/store';
 import { findOrCreateFolder } from './utils/find-or-create-folder';
+
+const REPORT_INTERVAL = 1000;
+
+type DownloadChunkParams = {
+  url: string;
+  start: number;
+  end: number;
+  index: number;
+  tempFilePath: string;
+  abortController: AbortController;
+  progressCallback: (index: number, loaded: number, total?: number) => void;
+};
+
+async function downloadChunk({
+  url,
+  start,
+  end,
+  index,
+  tempFilePath,
+  abortController,
+  progressCallback,
+}: DownloadChunkParams) {
+  const headers = {
+    Range: `bytes=${start}-${end}`,
+    Authorization: `Bearer ${getApiKey()}`,
+  };
+
+  const response = await axios.get(url, {
+    headers,
+    responseType: 'arraybuffer',
+    signal: abortController.signal,
+    onDownloadProgress: (progressEvent) => {
+      const { loaded, total } = progressEvent;
+      progressCallback(index, loaded, total);
+    },
+  });
+
+  fs.writeFileSync(`${tempFilePath}.part${index}`, response.data);
+}
+
+async function getFileSize(url: string) {
+  const CancelToken = axios.CancelToken;
+  const source = CancelToken.source();
+
+  try {
+    const response = await axios.get(url, {
+      cancelToken: source.token,
+      responseType: 'stream',
+      headers: {
+        Authorization: `Bearer ${getApiKey()}`,
+      },
+    });
+
+    // Get the content-length from the headers
+    const contentLength = response.headers['content-length'];
+    source.cancel('Got the content length, aborting the request');
+    return parseInt(contentLength, 10);
+  } catch (error) {
+    if (axios.isCancel(error)) {
+      console.log('Request canceled:', error.message);
+      return;
+    } else {
+      throw error;
+    }
+  }
+}
 
 type VaultDownloadParams = {
   resource: {
@@ -17,77 +83,119 @@ type VaultDownloadParams = {
   downloadPath: string;
 };
 
-// Update params
 export async function vaultDownload({
   downloadPath,
   resource,
 }: VaultDownloadParams) {
   const mainWindow = getWindow();
-  const controller = new AbortController();
-  const { data, headers } = await axios({
-    url: resource.url,
-    method: 'GET',
-    responseType: 'stream',
-    signal: controller.signal,
-    headers: {
-      Authorization: `Bearer ${getApiKey()}`,
-    },
-  });
-  const totalLength = parseInt(headers['content-length'], 10);
 
-  let elapsed_time = 0;
-  const start_time = Date.now();
-  let current = 0;
-  let speed = current / elapsed_time;
-  let remaining_time = (totalLength - current) / speed;
-  let progress = (current / totalLength) * 100;
-  let downloaded = 0;
+  // Number of parts to split into from settings
+  const NUMBER_PARTS = getSettings().concurrent || 10;
+  const startTime = performance.now();
+  let lastReportedTime = Date.now();
+
+  const fileSize = await getFileSize(resource.url);
+
+  // If the file size is not available, return (maybe throw an error here?)
+  if (!fileSize) return;
+
+  const controller = new AbortController();
+  const chunkSize = Math.ceil(fileSize / NUMBER_PARTS);
+
+  // Final file path
   const dirPath = path.resolve(__dirname, '', downloadPath);
-  const tempDirPath = path.resolve(getRootResourcePath(), 'tmp');
-  const tempFileName = uuid();
-  const tempFilePath = path.resolve(tempDirPath, tempFileName);
   const filePath = path.resolve(dirPath, resource.name);
-  const REPORT_INTERVAL = 1000;
-  let last_reported_time = Date.now();
+
+  // Temp file path
+  const tempFileName = uuid();
+  const tempDirPath = path.resolve(getRootResourcePath(), 'tmp');
+  const tempFilePath = path.resolve(tempDirPath, tempFileName);
 
   // Creates temp folder if it doesnt exist, also ensures that the main dir exists
   if (!fs.existsSync(tempDirPath)) {
     fs.mkdirSync(tempDirPath, { recursive: true });
   }
 
-  const writer = fs.createWriteStream(tempFilePath);
+  function cancelDownload(id: number) {
+    if (resource.id === id) {
+      console.log('Download canceled', resource.id);
 
-  data.on('data', (chunk) => {
-    const current_time = Date.now();
-    downloaded += chunk.length;
-    elapsed_time = current_time - start_time;
-    speed = (downloaded / elapsed_time) * 1024;
-    remaining_time = (totalLength - downloaded) / speed;
-    progress = (downloaded / totalLength) * 100;
+      // Abort download w/ Axios
+      controller.abort();
 
-    if (current_time - last_reported_time > REPORT_INTERVAL) {
+      mainWindow.setProgressBar(-1);
+    }
+  }
+
+  ipcMain.once('cancel-vault-download', (_, id) => cancelDownload(id));
+
+  let downloadedBytes = 0;
+  const progress = Array(NUMBER_PARTS).fill(0);
+
+  const updateProgress = (index: number, loaded: number) => {
+    const currentTime = Date.now();
+    downloadedBytes += loaded - progress[index];
+    progress[index] = loaded;
+
+    const elapsedTime = (performance.now() - startTime) / 1000; // seconds
+    const speed = downloadedBytes / elapsedTime; // bytes per second
+    const remainingTime = (fileSize - downloadedBytes) / speed; // seconds
+
+    if (currentTime - lastReportedTime > REPORT_INTERVAL) {
+      const totalProgress = (downloadedBytes / fileSize) * 100;
+
       // Updates the UI with the current progress
       mainWindow.webContents.send(`vault-download:${resource.id}`, {
-        totalLength,
-        downloaded,
-        progress,
+        totalLength: fileSize,
+        downloaded: downloadedBytes,
+        progress: totalProgress,
         speed,
-        remainingTime: remaining_time,
+        remainingTime,
         downloading: true,
       });
 
       // Updates the progress bar
-      mainWindow.setProgressBar(downloaded / totalLength);
+      mainWindow.setProgressBar(downloadedBytes / fileSize);
 
-      last_reported_time = current_time;
+      lastReportedTime = currentTime;
     }
-  });
+  };
 
-  data.on('end', async function () {
-    findOrCreateFolder(path.dirname(filePath));
+  findOrCreateFolder(path.dirname(filePath));
 
-    fs.renameSync(tempFilePath, filePath);
+  const promises: Promise<void>[] = [];
 
+  // Create the promises for each chunk
+  for (let i = 0; i < NUMBER_PARTS; i++) {
+    const start = i * chunkSize;
+    const end =
+      (i + 1) * chunkSize - 1 < fileSize
+        ? (i + 1) * chunkSize - 1
+        : fileSize - 1;
+
+    promises.push(
+      downloadChunk({
+        url: resource.url,
+        start,
+        end,
+        index: i,
+        progressCallback: updateProgress,
+        tempFilePath,
+        abortController: controller,
+      }),
+    );
+  }
+
+  await Promise.all(promises);
+
+  const writeStream = fs.createWriteStream(filePath);
+  for (let i = 0; i < NUMBER_PARTS; i++) {
+    const chunkPath = `${tempFilePath}.part${i}`;
+    await readStreamSync(chunkPath, writeStream);
+    fs.unlinkSync(chunkPath); // Clean up chunk file after merging
+  }
+
+  async function onEnd() {
     new Notification({
       title: 'Download Complete',
       body: resource.name,
@@ -98,30 +206,27 @@ export async function vaultDownload({
 
     // Updates the UI with the final progress
     mainWindow.webContents.send(`vault-download:${resource.id}`, {
-      totalLength,
-      downloaded,
+      totalLength: fileSize,
       progress: 100,
-      speed,
-      remainingTime: remaining_time,
       downloading: false,
     });
-  });
-
-  data.pipe(writer);
-
-  function cancelDownload(id: number) {
-    if (resource.id === id) {
-      console.log('Download canceled', resource.id);
-
-      // Abort download w/ Axios
-      controller.abort();
-
-      mainWindow.setProgressBar(-1);
-
-      // Remove from temp folder
-      fs.unlinkSync(tempFilePath);
-    }
   }
 
-  ipcMain.once('cancel-vault-download', (_, id) => cancelDownload(id));
+  writeStream.end(onEnd);
+
+  const totalTime = (performance.now() - startTime) / 1000;
+  console.log(`Total time: ${totalTime.toFixed(2)} seconds`);
+}
+
+function readStreamSync(chunkPath: string, writeStream: fs.WriteStream) {
+  const stream = fs.createReadStream(chunkPath);
+
+  return new Promise<void>((resolve) => {
+    stream.on('data', (data) => {
+      writeStream.write(data);
+    });
+    stream.on('end', () => {
+      resolve();
+    });
+  });
 }
